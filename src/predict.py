e198617478028c3cf803461fe0d65083bb1c466a
Gagan-2005 <160098907+Gagan-2005@users.noreply.github.com>
2025-07-09 12:05:51 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,11 @@
 # predict.py
 import joblib
 import numpy as np
+import os
 
-# Load model
-model = joblib.load("iris_model.pkl")
+# Load model from the correct path
+model_path = os.path.join("model", "iris_model.pkl")
+model = joblib.load(model_path)
 
 # Example input: [sepal_length, sepal_width, petal_length, petal_width]
 sample = np.array([[5.1, 3.5, 1.4, 0.2]])
